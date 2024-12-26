3278b24910e55bd3c54d5a82de5e9bbbb378d834
Re-codeUI <recodeui@gmail.com>
2024-12-26 23:15:19 +0700

    Remove All & Rebuild

---
+++ src/Providers/LaraQuickKitServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MagicBox\LaraQuickKit\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraQuickKitServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register bindings or configurations
+    }
+
+    public function boot()
+    {
+        // Bootstrapping logic like routes, migrations, etc.
+    }
+}
---
+++ tests/Test.php
@@ -0,0 +1,8 @@
+<?php
+namespace MagicBox\LaraQuickKit;
+
+class Test {
+    public static function hello() {
+        return 'LaraQuickKit is working!';
+    }
+}
